37396bbfc2ae6491f6acf35d3437d877b98acdf8
Steve Schofield <stevebschofield@gmail.com>
2017-01-31 14:17:23 +0000

    initial config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAF4X95tCaIBALqyKw8pRS4C6ZAmPi4RXEg4phy9iuAZCvH7YF9NCZBu6Hw97PZBDE6ICOhqpcddlFw0R6YcdL2aUsD59ViZAkJLHIOqDXLLivbMVytDFnsbqVow1lkWuLXKYZBDsZBMqMZB7c40Ss5eVLhML4MrJMs8ZAttfuGCkowZDZD',
+    FB_VERIFY_TOKEN: 'rasmussen_to_asgard',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ac4e79b4932543f79ff78fdf292e2047',
+    FB_APP_SECRET: '24a6af57feecf1516e15f37f5e0c1b4b',
+    SERVER_URL: "https://meetulf.herokuapp.com/",
 };
