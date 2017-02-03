e4523a23d4ee81d6ab30fe3b3b4ce3b7afc04126
Steve Schofield <stevebschofield@gmail.com>
2017-02-03 13:10:28 +0000

    refactor verify

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const crypto = require('crypto');
 const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -30,8 +30,33 @@ if (!config.SERVER_URL) { //used for ink to static files
 
 app.set('port', (process.env.PORT || 5000));
 
-//verify request came from facebook
-let verifyRequestSignature = fbMessageHandler.verifyRequestSignature;
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+const verifyRequestSignature = (req, res, buf) => {
+	const signature = req.headers['x-hub-signature'];
+
+	if (!signature) {
+		throw new Error('Couldn\'t validate the signature.');
+	} else {
+		const elements = signature.split('=');
+		// const method = elements[0];
+		const signatureHash = elements[1];
+
+		const expectedHash = crypto.createHmac('sha1', config.FB_APP_SECRET)
+			.update(buf)
+			.digest('hex');
+
+		if (signatureHash !== expectedHash) {
+			throw new Error('Couldnt validate the request signature.');
+		}
+	}
+};
 
 app.use(bodyParser.json({
 	verify: verifyRequestSignature
--- src/fbMessageHandler.js
+++ src/fbMessageHandler.js
@@ -1,4 +1,3 @@
-const crypto = require('crypto');
 const uuid = require('uuid');
 const config = require('../config');
 const helpers = require('../helpers/helper');
@@ -185,40 +184,11 @@ const receivedAuthentication = (event) => {
 	fbMessageType.sendTextMessage(senderID, 'Authentication successful');
 };
 
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-const verifyRequestSignature = (req, res, buf) => {
-	const signature = req.headers['x-hub-signature'];
-
-	if (!signature) {
-		throw new Error('Couldn\'t validate the signature.');
-	} else {
-		const elements = signature.split('=');
-		// const method = elements[0];
-		const signatureHash = elements[1];
-
-		const expectedHash = crypto.createHmac('sha1', config.FB_APP_SECRET)
-			.update(buf)
-			.digest('hex');
-
-		if (signatureHash !== expectedHash) {
-			throw new Error('Couldnt validate the request signature.');
-		}
-	}
-};
-
 module.exports = {
 	receivedMessage,
 	receivedPostback,
 	receivedMessageRead,
 	receivedAuthentication,
-	verifyRequestSignature,
 	receivedDeliveryConfirmation,
 	receivedAccountLink
 };
