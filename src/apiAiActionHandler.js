d7190eff661a4ec7a67192fadc6e3fc6fa11ceab
Steve Schofield <stevebschofield@gmail.com>
2017-02-04 10:35:39 +0000

    testing action handler

--- src/apiAiActionHandler.js
+++ src/apiAiActionHandler.js
@@ -1,8 +1,19 @@
+const helpers = require('../helpers/helper');
 const fbMessageType = require('../managers/fbMessageTypesManager');
 
 const handleApiAiAction = (sender, action, responseText, contexts, parameters) => {
 	switch (action) {
-	case 0: {
+	case 'personalised-welcome': {
+		if (helpers.isDefined(contexts[0] && contexts[0].name == 'default-welcome')) {
+			fbMessageType.messageManager.sendTextMessage(sender, 'Hi there');
+			fbMessageType.messageManager.sendTypingOn(sender);
+			setTimeout(function(){
+
+			}, 2000);
+
+		} else {
+			fbMessageType.messageManager.sendTextMessage(sender, 'Bonjour');
+		}
 		break;
 	}
 	default:
