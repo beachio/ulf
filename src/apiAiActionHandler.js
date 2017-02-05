6c0b9860145c1b80e9ff0a848370f3fd860af8de
Steve Schofield <stevebschofield@gmail.com>
2017-02-05 10:52:07 +0000

    set user on welcome

--- src/apiAiActionHandler.js
+++ src/apiAiActionHandler.js
@@ -1,4 +1,5 @@
 const helpers = require('../helpers/helper');
+const dataStore = require('../services/dataStoreService');
 const fbMessageType = require('../managers/fbMessageTypesManager');
 const fbProfile = require('../services/fbProfileService');
 const onboarding = require('../stories/onboarding');
@@ -7,6 +8,7 @@ const handleApiAiAction = (sender, action, responseText, contexts, parameters) =
 	switch (action) {
 	case 'personalised-welcome': {
 		onboarding.startOnboarding(sender);
+		dataStore.setUserInfo(sender);
 		break;
 	}
 	default:
